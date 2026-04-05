cf8aa741749090f1f1b92baca61c3eaf7ad3a6c1
IslemFou <fourati.islem@outlook.fr>
2026-04-05 14:53:28 +0200

    update route dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const {
     dashboardClient,
-    dashboardVendor,
     dashboardProvider,
     dashboardAdmin
 } = require('../controllers/dashboardController');
