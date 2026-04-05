c923e2e2018632da2c3ee97ce7370ff2f5787463
IslemFou <fourati.islem@outlook.fr>
2026-04-05 14:26:25 +0200

    update de routes/projects afin de l'adapter au projet

--- routes/project.js
+++ routes/project.js
@@ -3,29 +3,30 @@ const router = express.Router();
 const {
     getProjects,
     getProject,
-    createProject,
+    updateProgress,
     updateProject,
     deleteProject,
 } = require('../controllers/projectController');
 
-// Import your future auth middlewares
-// const { protect, authorize } = require('../middleware/auth');
-
-// For now, routes are public for testing, but they should be protected soon
+// Import auth middlewares
+const { protect, authorize } = require('../middleware/auth');
+// Application de protect à toutes les routes de ce fichier
+router.use(protect);
+//Au lieu d'écrire protect sur chaque ligne, le mettre en haut du fichier de routes (après les imports) protège toutes les routes qui suivent. C'est plus propre et tu n'oublieras jamais de sécuriser une nouvelle route.
 
 router
     .route('/')
     .get(getProjects) //Get all the projects (filtered by user in controller)
-    .post(createProject); //// Triggered when an Order is paid
 
 router
     .route('/:id')
     .get(getProject) // Get single project details
-    .put(updateProject);  //Update URLs, description, or status
+    .put(updateProject) //Update URLs, description, or status
+    .delete(deleteProject);
 
 // Special route for developers to quickly update % progress
+// On peut ajouter authorize('provider') ici pour plus de sécurité
 router.put('/:id/progress', updateProgress);
 
-router.delete(protect, deleteProject);
 
 module.exports = router;
